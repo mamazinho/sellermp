d4184fb03cf56f62984b263e6893de78de46b362
matheus <matheus.tessaroli@olist.com>
2021-06-08 17:48:27 -0300

    seller finished

--- seller/urls.py
+++ seller/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from seller.views import Seller, index
+from seller.views import Seller, Detail, index
 
 urlpatterns = [
     path('', index),
     path('sellers/', Seller.as_view(), name='sellers'),
     path('sellers/<action>', Seller.as_view(), name='sellers'),
     path('sellers/<action>/<id>', Seller.as_view(), name='sellers'),
+
+    path('seller-details/<action>', Detail.as_view(), name='seller-details'),
+    path('seller-details/<action>/<id>', Detail.as_view(), name='seller-details'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from seller.models import Seller as MSeller
+from seller.models import Seller as MSeller, SellerContact as MDetail
 
 # Create your views here.
 def index(request):
@@ -14,9 +14,10 @@ class Seller(View):
                 return render(request, 'sellers-form.html')
             elif action == 'update':
                 seller = MSeller.objects.get(id=id)
-                return render(request, 'sellers-form.html', {'seller': seller})
+                details = seller.contacts.all()
+                return render(request, 'sellers-form.html', {'seller': seller, 'details': details})
             elif action == 'delete':
-                self.delete(request, id)
+                return self.delete(request, id)
 
         sellers = MSeller.objects.all()
         return render(request, 'sellers.html', {'sellers': sellers})
@@ -51,6 +52,47 @@ class Seller(View):
         seller.save()
         return redirect('sellers')
 
-    def delete(self, request, id=None):
+    def delete(self, request, id):
         MSeller.objects.get(id=id).delete()
         return redirect('sellers')
+
+class Detail(View):
+    def get(self, request, action, id=None):
+        seller_id = request.GET.get('seller_id')
+        if action == 'new':
+            return render(request, 'details-form.html', {'seller_id': seller_id})
+        elif action == 'update':
+            detail = MDetail.objects.get(id=id)
+            return render(request, 'details-form.html', {'detail': detail, 'seller_id': seller_id})
+        elif action == 'delete':
+            return self.delete(request, id, seller_id)
+
+    def post(self, request, action=None, id=None):
+        data = request.POST
+        if id:
+            self.patch(request, id)
+        else:
+            MDetail.objects.create(
+                address=data.get('address'),
+                seller_id=data.get('seller_id'),
+                responsible_email=data.get('responsible_email'),
+                phone_number=data.get('phone_number'),
+            )
+
+        return redirect('sellers', action='update', id=data.get('seller_id'))
+
+
+    def patch(self, request, id=None):
+        data = request.POST
+        detail = MDetail.objects.get(id=id)
+
+        detail.address = data.get('address', detail.address)
+        detail.responsible_email = data.get('responsible_email', detail.responsible_email)
+        detail.phone_number = data.get('phone_number', detail.phone_number)
+
+        detail.save()
+        return redirect('sellers', action='update', id=data.get('seller_id'))
+
+    def delete(self, request, id, seller_id=None):
+        MDetail.objects.get(id=id).delete()
+        return redirect('sellers', action='update', id=seller_id)
